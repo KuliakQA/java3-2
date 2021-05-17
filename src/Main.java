7819d9ab7758f31fa4ff64be564f0d59a19ba86c
KuliakQA <culeacvadim@gmail.com>
2021-05-17 16:24:06 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        int account = 100; // RUB
+        int put = 1100; //RUB
+        int result = put;
+        int less = 1000;
+        int percent;
+        if (put >= less) {
+            percent = 1;
+        } else {
+            percent = 0;
+        }
+        long amount = result;
+        long bonus = amount * percent / 100;
+        System.out.println(bonus + amount + account);
+        System.out.println("Bonus is: " + bonus);
+    }
+}
